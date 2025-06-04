fef8be5b74ed881f9849aa489a6e7894cf07cb8d
AMOS KIPKORIR <amos.kipkorir@student.moringaschool.com>
2025-06-04 11:10:54 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+
+let numbers = [5, 12, 20, 7, 3];
+let max = Math.max(...numbers); // 20
